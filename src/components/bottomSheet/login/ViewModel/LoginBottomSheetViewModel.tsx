fe6502e39ed07b7f13d04a6071798c0f0f10dae6
OTIS <72382663+1domybest@users.noreply.github.com>
2025-02-23 20:04:57 +0900

    Update LoginBottomSheetViewModel.tsx

--- src/components/bottomSheet/login/ViewModel/LoginBottomSheetViewModel.tsx
+++ src/components/bottomSheet/login/ViewModel/LoginBottomSheetViewModel.tsx
@@ -34,7 +34,7 @@ class LoginBottomSheetViewModel {
     snsLogin(provider: string) {
 
         const loginWindow = window.open(
-            `${ServerConstants.SERVER_URL}/api/oauth2/authorization/${provider}`,
+            `${ServerConstants.SERVER_URL}/oauth2/authorization/${provider}`,
             "login",
             "width=auto,height=auto,top=100,left=100,location=no,resizable=yes,menubar=no,toolbar=no,status=no"
         );
