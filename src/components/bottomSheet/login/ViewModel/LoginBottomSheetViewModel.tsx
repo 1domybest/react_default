e0119ae7d4e0bb6f9186df26364bf94a33d42b71
OTIS <72382663+1domybest@users.noreply.github.com>
2025-03-02 19:52:42 +0900

    d

--- src/components/bottomSheet/login/ViewModel/LoginBottomSheetViewModel.tsx
+++ src/components/bottomSheet/login/ViewModel/LoginBottomSheetViewModel.tsx
@@ -31,11 +31,11 @@ class LoginBottomSheetViewModel {
 
     snsLogin(provider: string) {
 
-        // const loginWindow = window.open(
-        //     `${ServerConstants.SERVER_URL}/oauth2/authorization/${provider}`,
-        //     "login",
-        //     "width=auto,height=auto,top=100,left=100,location=no,resizable=yes,menubar=no,toolbar=no,status=no"
-        // );
+        const loginWindow = window.open(
+            `${ServerConstants.SERVER_URL}/oauth2/authorization/${provider}`,
+            "login",
+            "width=auto,height=auto,top=100,left=100,location=no,resizable=yes,menubar=no,toolbar=no,status=no"
+        );
 
         // const loginWindow = window.open(
         //     `http://localhost:8080/oauth2/authorization/${provider}`,
@@ -43,15 +43,15 @@ class LoginBottomSheetViewModel {
         //     "width=auto,height=auto,top=100,left=100,location=no,resizable=yes,menubar=no,toolbar=no,status=no"
         // );
 
-        window.location.href = `http://127.0.0.1:8080/oauth2/authorization/${provider}`;
+        // window.location.href = `http://127.0.0.1:8080/oauth2/authorization/${provider}`;
 
-        // const checkLoginStatus = setInterval(async () => {
-        //     if (loginWindow?.closed) {
-        //         console.log("자식 창이 닫혔습니다. 로그인 완료 확인 필요.");
-        //         clearInterval(checkLoginStatus);
-        //         await this.succeedSNSLogin();
-        //     }
-        // }, 500);
+        const checkLoginStatus = setInterval(async () => {
+            if (loginWindow?.closed) {
+                console.log("자식 창이 닫혔습니다. 로그인 완료 확인 필요.");
+                clearInterval(checkLoginStatus);
+                await this.succeedSNSLogin();
+            }
+        }, 500);
     }
 
     // SNS 로그인 성공 처리
